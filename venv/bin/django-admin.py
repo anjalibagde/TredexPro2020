ec2adf604780c38a6de015793444e0990d922cad
anjalibagde <60692696+anjalibagde@users.noreply.github.com>
2024-11-11 12:55:55 +0530

    Delete venv directory

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/root/tedxntua2020/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv/bin/manage.py
+++
@@ -1,7 +0,0 @@
-#!/root/tedxntua2020/venv/bin/python
-# EASY-INSTALL-DEV-SCRIPT: 'tedxntua2020===2020.10-prealpha','manage.py'
-__requires__ = 'tedxntua2020===2020.10-prealpha'
-__import__('pkg_resources').require('tedxntua2020===2020.10-prealpha')
-__file__ = '/root/tedxntua2020/manage.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
